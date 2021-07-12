60b0904597500f979488051715a83b3a79f18920
Hentioe <me@bluerain.io>
2021-07-12 23:25:59 +0800

    Update nix-shell config

--- .nix/magickwand.nix
+++ .nix/magickwand.nix
@@ -1,7 +1,7 @@
 with import <nixpkgs> { };
 
 stdenv.mkDerivation {
-  name = "magickwand-features-compile";
+  name = "features-magickwand-compile";
   buildInputs = [ pkg-config imagemagick llvmPackages_12.libclang.lib ];
 
   LIBCLANG_PATH = "${llvmPackages_12.libclang.lib}/lib";
